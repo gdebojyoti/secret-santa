66b5c3934f6126064eb70bb4845babafe4047f99
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2021-12-21 20:51:52 +0530

    ga setup

--- src/shared/index.js
+++ src/shared/index.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
   Switch,
   Route
 } from 'react-router-dom'
 
 import routes from './routes'
+import { initGa, trackingPageGa } from 'shared/utils/reactGa'
 
 const App = () => {
+  useEffect(() => {
+    initGa()
+    trackingPageGa(window.location.pathname)
+  })
   return (
     <Switch>
       {routes.map(({ path, component, isExact }, index) => (
